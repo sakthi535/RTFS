370aa5c8b8407e86eb1cc340e432ce214bb38c3e
S Sakthi Saravanan <71821856+sakthi535@users.noreply.github.com>
2022-04-30 22:50:43 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,73 @@
+(function(){
+
+	let recID;
+	const soc = io();
+
+	function generateID(){
+		return `${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}`;
+	}
+
+	document.querySelector("#sender-start-con-btn").addEventListener("click",function(){
+		let joinID = generateID();
+		document.querySelector("#join-id").innerHTML = `
+			<b>Room ID</b>
+			<span>${joinID}</span>
+		`;
+		soc.emit("sender-join", {
+			uid:joinID
+		});
+	});
+
+	soc.on("init",function(uid){
+		recID = uid;
+		document.querySelector(".join-screen").classList.remove("active");
+		document.querySelector(".fs-screen").classList.add("active");
+	});
+
+	document.querySelector("#file-input").addEventListener("change",function(e){
+		let file = e.target.files[0];
+		if(!file){
+			return;		
+		}
+		let reader = new FileReader();
+		reader.onload = function(e){
+			let buffer = new Uint8Array(reader.result);
+
+			let el = document.createElement("div");
+			el.classList.add("item");
+			el.innerHTML = `
+					<div class="progress">0%</div>
+					<div class="filename">${file.name}</div>
+			`;
+			document.querySelector(".files-list").appendChild(el);
+			shareFile({
+				filename: file.name,
+				total_buffer_size:buffer.length,
+				buffer_size:2048,
+			}, buffer, el.querySelector(".progress"));
+		}
+		reader.readAsArrayBuffer(file);
+	});
+
+
+	function shareFile(metadata,buffer,progress_node){
+		soc.emit("file-meta", {
+			uid:recID,
+			metadata:metadata
+		});
+		
+		soc.on("fs-share",function(){
+			let chunk = buffer.slice(0,metadata.buffer_size);
+			buffer = buffer.slice(metadata.buffer_size,buffer.length);
+			progress_node.innerText = Math.trunc(((metadata.total_buffer_size - buffer.length) / metadata.total_buffer_size * 100));
+			if(chunk.length != 0){
+				soc.emit("file-raw", {
+					uid:recID,
+					buffer:chunk
+				});
+			} else {
+				console.log("Sent file successfully");
+			}
+		});
+	}
+})();

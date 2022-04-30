2fa98e2c96516bdfcc40b317efc8098b12494b72
S Sakthi Saravanan <71821856+sakthi535@users.noreply.github.com>
2022-04-30 22:55:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname+"/public")));
+app.use(express.static(path.join(__dirname+"/")));
 
 io.on("connection", function(soc){
 	soc.on("join-server",function(data){
